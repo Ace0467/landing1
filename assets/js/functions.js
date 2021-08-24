d24b8775d9592eb367ce36d77ee7cd7bed81ce23
Ace0467 <72983987+Ace0467@users.noreply.github.com>
2021-08-24 19:51:00 -0300

    commit1

---
+++ assets/js/functions.js
@@ -0,0 +1,6 @@
+function show(){
+   let list = document.querySelector("#trust-list")
+   let list2 = document.querySelector("#trust-list2")
+   list.style.display = "none"
+   list2.style.display = "flex"
+}
